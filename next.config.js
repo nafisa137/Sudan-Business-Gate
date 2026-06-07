fb0e7a9d1e8386a3869b5e3ec4b5949bbcda1be2
Nafisa Abaalrahman <97001917+nafisa137@users.noreply.github.com>
2026-06-07 15:32:06 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
+}
+
+module.exports = nextConfig
